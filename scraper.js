41b1d42b8d1ca2004f81d6a4ab7de84f603ad1f0
RWhite007 <96767003+RWhite007@users.noreply.github.com>
2022-01-13 14:50:48 -0800

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -4,9 +4,6 @@ const myUrl = 'https://www.newegg.com/msi-geforce-rtx-3080-rtx-3080-ventus-3x-10
 const {Client, Intents} = require('discord.js');
 const config = require('./config.json'); //Discord bot token
 
-const client = new Client({  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGES]  });
-
-   
 async function webScraper(url) {
    
     while (true) {
@@ -23,20 +20,21 @@ async function webScraper(url) {
         console.log('Not in stock, checking again soon');
         await sleep ((Math.random() * 1000));  //pauses before closing browser
         browser.close();
+        const client = new Client({  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGES]  });
         client.once('ready', () => {
             console.log('Discord client open and ready');
             const channel = client.channels.cache.get('929197937924407309').send(`3080 in stock!  Here's the link ${myUrl}`);
+            client.logout;
         });
+        
         client.login(config.BOT_TOKEN);
         await sleep((Math.random() * 3000) + 4000); 
 
     } else {  
         console.log('GPU in stock!?');
         await sleep(3600000);
-        
-    }
+        }
 }
-    
 }
 
 function sleep (ms) {
